f6d2896596be99834fa0f9ee9680e7bb777b74b5
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2024-03-14 16:25:28 +0000

    check errors when login in

--- apps/k6-tests/src/graphql/support/user.ts
+++ apps/k6-tests/src/graphql/support/user.ts
@@ -14,6 +14,12 @@ export class User {
       externalTokenLogin(redirectUri: " ", externalToken: "${sessionId}")
      }`;
     const response = this.apiClient(JSON.stringify({ query }));
+    if (response.status !== 200) {
+      throw new Error(
+        `Error response getUserToken ${response.status} ${response?.body} ${response?.error} ${response?.error_code}`
+      );
+    }
+
     const responseData = response.json() as ExternalTokenLoginResponse;
     if (
       !check(response, {
