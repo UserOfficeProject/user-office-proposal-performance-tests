763f820429cfbe33fba7f12d9a0fec8168454c0f
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2024-09-05 15:46:25 +0100

    add build from sh script

--- build-push-image.sh
+++ build-push-image.sh
@@ -7,5 +7,5 @@ VERSION_TAG=$2
 BUILD_DIR=$3
 
 TAG=$(echo "$BRANCH_NAME" | tr _ -)
-#docker build --pull -t harbor.stfc.ac.uk/isisbusapps/$IMAGE_NAME:$VERSION_TAG $BUILD_DIR 
-#docker push harbor.stfc.ac.uk/isisbusapps/$IMAGE_NAME:$VERSION_TAG 
+docker build --pull -t harbor.stfc.ac.uk/isisbusapps/$IMAGE_NAME:$VERSION_TAG $BUILD_DIR 
+docker push harbor.stfc.ac.uk/isisbusapps/$IMAGE_NAME:$VERSION_TAG 
