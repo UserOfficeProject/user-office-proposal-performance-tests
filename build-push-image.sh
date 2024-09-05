1eaa6bbdfd3c6203adf88be0d4a31ac534b06cee
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2024-09-05 14:28:02 +0100

    add k6 and test setup build and push for cluster images

---
+++ build-push-image.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Helper script for building and pushing docker images.
+
+IMAGE_NAME=$1
+VERSION_TAG=$2
+BUILD_DIR=$3
+
+TAG=$(echo "$BRANCH_NAME" | tr _ -)
+docker build --pull -t harbor.stfc.ac.uk/isisbusapps/$IMAGE_NAME:$VERSION_TAG $BUILD_DIR 
+docker push harbor.stfc.ac.uk/isisbusapps/$IMAGE_NAME:$VERSION_TAG 
