4f2e280aa0e8a504e78939ecebf4bc9d76d86fe2
Rasmia Kulan <rasmia.kulan@stfc.ac.uk>
2024-11-26 11:57:52 +0000

    update endpoint to remove role from user

--- src/UOWSClient.ts
+++ src/UOWSClient.ts
@@ -1,7 +1,7 @@
 import { UOWS } from './generated';
 
 const UOWSClient = new UOWS({
-  BASE: 'http://localhost:4008/users-service',
+  BASE: 'http://user-office-web-service:8080/users-service',
   HEADERS: {
     Authorization: `Api-key 93391b02-ead7-4c7d-ba3e-687adf269906`,
   },
--- src/support/setup.ts
+++ src/support/setup.ts
@@ -108,7 +108,7 @@ export async function sc1Setup(environmentConfig: EnvironmentConfigurations) {
       }
     }
   }
-  //
+
   // Check for final setup outcome and abort if necessary
   if (!proposalHealthCheck) {
     console.error(
