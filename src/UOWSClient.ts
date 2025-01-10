f7c93ba235cf81bf9f08d3dcc2d3e4a676377bc4
Bhaswati Dey <bhaswati1148@gmail.com>
2025-01-10 12:53:56 +0000

    moving the openapi yaml and uows client inside test-setup

--- src/UOWSClient.ts
+++
@@ -1,9 +0,0 @@
-import { UOWS } from './generated';
-
-const UOWSClient = new UOWS({
-  BASE: 'http://user-office-web-service:8080/users-service',
-  HEADERS: {
-    Authorization: `Api-key 93391b02-ead7-4c7d-ba3e-687adf269906`,
-  },
-});
-export default UOWSClient;
