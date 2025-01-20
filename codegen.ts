33af977358e038b45d830ccf9f3ed4712f23ce69
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2025-01-20 17:33:19 +0000

    fix codegen file

--- codegen.ts
+++ codegen.ts
@@ -3,16 +3,17 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   schema: `${process.env.GRAPHQL_URL ||'http://localhost:4000/graphql'}`,
   documents: [
-    'src/graphql/**/*.ts',
-    'src/graphql/**/*.graphql',
-    'src/graphql/**/*.gql'
+    'src/graphql/gql/*.ts',
+    'src/graphql/gql/*.graphql',
+    'src/graphql/gql/*.gql'
   ],
-  ignoreNoDocuments: true,
   overwrite:true,
   generates: {
     './src/graphql/generated/': {
       preset: 'client',
+     
       config: {
+        plugins: ['typescript', 'typescript-operations', 'typed-document-node'],
         documentMode: 'string',
       },
     },
