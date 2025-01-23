49a95bafa63e1b03765743f4500a8e5605bb6be9
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2025-01-23 13:05:34 +0000

    update webpack to use node target

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,43 @@
 import { join } from 'path';
 import { resolve as _resolve } from 'path';
-import { fileURLToPath } from 'url'; 
+import { fileURLToPath } from 'url';
 import path from 'path';
 import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import GlobEntries from 'webpack-glob-entries';
 
-
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); 
+const __dirname = path.dirname(__filename);
 
 export default {
-    mode: 'production',
-    entry: GlobEntries('./src/*test*.ts'),
-    experiments: {
-        outputModule: true,
-    },
-    output: {
-        path: join(__dirname, 'test'),
-        libraryTarget: 'commonjs',
-        filename: '[name].js',
-    },
-    resolve: {
-      extensions: ['.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    target: 'web',
-    externals: /^(k6|https?\:\/\/)(\/.*)?/,
-    stats: {
-      colors: true,
-    },
-    plugins: [new CleanWebpackPlugin()],
-    optimization: {
-      minimize: false,
-    },
-  };
+  mode: 'production',
+  entry: GlobEntries('./src/**/*test*.ts'),
+  experiments: {
+    outputModule: true,
+  },
+  output: {
+    path: join(__dirname, 'test'),
+    libraryTarget: 'commonjs',
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  target: 'node',
+  externals: /^(k6|https?\:\/\/)(\/.*)?/,
+  stats: {
+    colors: true,
+  },
+  plugins: [new CleanWebpackPlugin()],
+  optimization: {
+    minimize: false,
+  },
+};
