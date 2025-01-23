912d085091bdc839a48b761b4b91a018e21628f1
Bhaswati Dey <bhaswati1148@gmail.com>
2025-01-23 15:37:09 +0000

    update variable names for uows  base url and api key

--- test-setup/src/client/UOWSClient.ts
+++ test-setup/src/client/UOWSClient.ts
@@ -1,9 +1,9 @@
 import { UOWS } from '../../generated';
 
 const UOWSClient = new UOWS({
-  BASE: process.env.BASE_URL,
+  BASE: process.env.UOWS_DEV_BASE_URL,//to run in local environment use process.env.BASE_URL and process.env.API_KEY
   HEADERS: {
-    Authorization: `Api-key ${process.env.API_KEY}`,
+    Authorization: `Api-key ${process.env.UOWS_DEV_API_KEY}`,
   },
 });
 export default UOWSClient;
