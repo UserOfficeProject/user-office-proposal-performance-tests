5c7e97f86bb8bfee5241c45386bcfc01f2cd3675
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2025-01-23 13:36:10 +0000

    use browser crypto

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,16 +1,8 @@
 import { FsFile, UserLogin } from './sharedType';
 
-import { randomBytes } from 'crypto';
-
 export function randomUUIDv4(): string {
-  const bytes = randomBytes(16);
-  bytes[6] = (bytes[6] & 0x0f) | 0x40; // Set the version to 4
-  bytes[8] = (bytes[8] & 0x3f) | 0x80; // Set the variant to 10
-
-  const hexBytes = Array.from(bytes).map(byte => byte.toString(16).padStart(2, '0')).join('');
-  return `${hexBytes.slice(0, 8)}-${hexBytes.slice(8, 12)}-${hexBytes.slice(12, 16)}-${hexBytes.slice(16, 20)}-${hexBytes.slice(20)}`;
+  return crypto.randomUUID()
 }
-
 export function randomIntBetween(min: number, max: number): number {
   // Ensure correct inclusive behavior:
   return Math.floor(Math.random() * (max - min + 1)) + min;
