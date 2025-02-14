e9696374958525fba00d030e8a611e1dbfb56059
Farai Mutambara <farai.mutambara@stfc.ac.uk>
2025-02-14 17:21:19 +0000

    and wait to deployment delete

--- cluster-cli/index.ts
+++ cluster-cli/index.ts
@@ -3,25 +3,36 @@ import { getQuestions } from './src/questions';
 import { processQuestions } from './src/support';
 import { deleteDeployment, deleteTestRunObjects } from './src/deployments';
 import { deleteConfigMap } from './src/configmap';
-const testTestUpDeployment = 'test-setup-deployment';
+import * as k8s from '@kubernetes/client-node';
+
 const namespace = 'apps';
 const group = 'k6.io';
 const version = 'v1alpha1';
-const plural = 'testruns'
+const plural = 'testruns';
+const testSetupLabel = 'app=test-setup';
+const testSetupDeployment = 'test-setup-deployment';
+
 const main = async () => {
   try {
+    await deleteDeployment(testSetupDeployment, namespace, testSetupLabel);
+
     const questions = await getQuestions();
     const answers = await processQuestions(questions);
     //Remove previous deployments
-    await deleteDeployment(namespace, testTestUpDeployment);
-    await deleteTestRunObjects(`${String(answers.K6_TEST_NAME)}-deployment`,namespace,group,version,plural)
+    await deleteDeployment(testSetupDeployment, namespace, testSetupLabel);
+    await deleteTestRunObjects(
+      `${String(answers.K6_TEST_NAME)}-deployment`,
+      namespace,
+      group,
+      version,
+      plural
+    );
     //Remove configs
     await deleteConfigMap('test-scripts', namespace);
     await deleteConfigMap('test-fixtures', namespace);
     //runClusterTest(answers);
   } catch (error) {
-    console.log(`Error executing cli ${error}`);
-    console.log(chalk.green('Previous test set up deleted'));
+    console.log(chalk.red(`Error executing cli ${error}`));
     process.exit();
   }
 };
--- cluster-cli/src/deployments.ts
+++ cluster-cli/src/deployments.ts
@@ -6,35 +6,65 @@ const kc = new k8s.KubeConfig();
 kc.loadFromDefault();
 const appsV1Api = kc.makeApiClient(k8s.AppsV1Api);
 const customV1Api = kc.makeApiClient(k8s.CustomObjectsApi);
+const coreV1Api = kc.makeApiClient(k8s.CoreV1Api);
 const spinner = ora();
 
-export async function deleteDeployment(name: string, namespace: string) {
+export async function deleteDeployment(
+  name: string,
+  namespace: string,
+  labelSelector: string
+) {
   spinner.start(`Deleting ${name} deployment from namespace "${namespace}"`);
-  return await appsV1Api
-    .deleteNamespacedDeployment({ name, namespace })
-    .then(() => {
-      spinner.succeed(
-        `Deployment "${name}" deleted from namespace "${namespace}"`
-      );
-    })
-    .catch((error) => {
-      if (error && !String(error).includes('not found')) {
-        spinner.fail(
-          `Failed to delete ${name} deployment from namespace "${namespace}"`
-        );
-        throw new Error(error);
-      } else {
+  const podList = await coreV1Api.listNamespacedPod({
+    namespace,
+    labelSelector,
+  });
+  if (!podList.items.length) {
+    spinner.succeed(`Deployment "${name}" already deleted form "${namespace}"`);
+  }
+  if (podList.items.length > 0) {
+    let podsDeleted = false;
+    const timeout = 60 * 1000;
+    const startTime = Date.now();
+
+    await appsV1Api.deleteNamespacedDeployment({
+      name,
+      namespace,
+    });
+    while (!podsDeleted && Date.now() - startTime < timeout) {
+      const podList = await coreV1Api.listNamespacedPod({
+        namespace,
+        labelSelector,
+      });
+      if (podList.items.length === 0) {
+        podsDeleted = true;
         spinner.succeed(
           `Deployment "${name}" deleted from namespace "${namespace}"`
         );
+        break;
       }
-    });
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // Check every 1 second
+    }
+    if (!podsDeleted) {
+      spinner.fail(
+        `Failed to delete ${name} deployment from namespace "${namespace}"`
+      );
+      throw new Error('Timeout waiting for pods to be deleted.');
+    }
+  }
+  return;
 }
 
-export async function deleteTestRunObjects(name: string, namespace: string,group: string,version: string,plural: string) {
-
+export async function deleteTestRunObjects(
+  name: string,
+  namespace: string,
+  group: string,
+  version: string,
+  plural: string
+) {
   spinner.start(`Deleting ${name} TestRun from namespace "${namespace}"`);
-  return await customV1Api.deleteNamespacedCustomObject({name,group,version,plural,namespace})
+  return await customV1Api
+    .deleteNamespacedCustomObject({ name, group, version, plural, namespace })
     .then(() => {
       spinner.succeed(
         `TestRun "${name}" deleted from namespace "${namespace}"`
