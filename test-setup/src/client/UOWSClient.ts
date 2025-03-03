82613f8a14f432745f8da1d2b57b4f999445ac18
Bhaswati Dey <bhaswati1148@gmail.com>
2025-03-03 09:53:42 +0000

    fix further lint issues

--- test-setup/src/client/UOWSClient.ts
+++
@@ -1,9 +0,0 @@
-import { UOWS } from '../../generated';
-
-const UOWSClient = new UOWS({
-  BASE: process.env.UOWS_DEV_BASE_URL,//to run in local environment use process.env.BASE_URL and process.env.API_KEY
-  HEADERS: {
-    Authorization: `Api-key ${process.env.UOWS_DEV_API_KEY}`,
-  },
-});
-export default UOWSClient;
